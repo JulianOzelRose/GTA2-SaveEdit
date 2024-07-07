20af272669a6d3cd7ce3957b59b7590319844e28
Julian Ozel Rose <julian.ozel.rose@outlook.com>
2024-07-07 03:37:01 -0700

    Update version to 1.04

--- GTA2-SaveEdit/AboutForm.Designer.cs
+++ GTA2-SaveEdit/AboutForm.Designer.cs
@@ -154,7 +154,7 @@ namespace GTA2_SaveEdit
             this.lblVersion.Name = "lblVersion";
             this.lblVersion.Size = new System.Drawing.Size(79, 17);
             this.lblVersion.TabIndex = 13;
-            this.lblVersion.Text = "Version 1.03";
+            this.lblVersion.Text = "Version 1.04";
             // 
             // lblAbout6
             // 
